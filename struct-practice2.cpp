6aac878b642a719b3a0ba2222e5082fd0f9cfc82
Yuannzh1 <165546950+Yuannzh1@users.noreply.github.com>
2024-05-03 10:43:00 +0800

    Update struct-practice2.cpp

--- struct-practice2.cpp
+++ struct-practice2.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
-struct point {
-int point;
+struct point {//結構標籤
+int point; //結構內成員
 };
 typedef struct point t_point;
 int main()
 {
-int point;
+int point; //結構外名稱
 t_point p;
 cin >> point;
 p.point = point;
 cout<<"p.point: "<<p.point<<endl;
 return 0;
 }
+//總結：結構內的成員變數名稱與外部的變數名稱是可以相同的。
+//總結：結構標籤與結構成員的名稱是可以相同的
