1ba4ce61a95c91ab506d6b7583405388bc4c035f
Yuannzh1 <165546950+Yuannzh1@users.noreply.github.com>
2024-05-07 15:09:25 +0800

    Create 002_switch.cpp

---
+++ 002_switch.cpp
@@ -0,0 +1,11 @@
+switch (expression) {
+    case value1:
+        // 如果 expression 的值等於 value1，執行這裡的程式碼
+        break;
+    case value2:
+        // 如果 expression 的值等於 value2，執行這裡的程式碼
+        break;
+    // 可以有更多的 case
+    default:
+        // 如果 expression 的值不等於任何 case 中的值，執行這裡的程式碼
+}
