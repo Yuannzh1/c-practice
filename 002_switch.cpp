0b3b8e26d4ca6c9ca62f67031d03497a0efdd922
Yuannzh1 <165546950+Yuannzh1@users.noreply.github.com>
2024-05-07 15:18:23 +0800

    Update 002_switch.cpp

--- 002_switch.cpp
+++ 002_switch.cpp
@@ -9,3 +9,22 @@ switch (expression) {
     default:
         // 如果 expression 的值不等於任何 case 中的值，執行這裡的程式碼
 }
+    switch (grade) {
+        case 'A':
+            cout << "Excellent!" << endl;
+            break;
+        case 'B':
+            cout << "Good job!" << endl;
+            break;
+        case 'C':
+            cout << "Not bad!" << endl;
+            break;
+        case 'D':
+            cout << "You passed, but you need to improve." << endl;
+            break;
+        case 'F':
+            cout << "You failed. Please study harder." << endl;
+            break;
+        default:
+            cout << "Invalid grade entered." << endl;
+    }
