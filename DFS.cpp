70d67a637a6084675816f2d8c91f019ad3ce5389
Yuannzh1 <165546950+Yuannzh1@users.noreply.github.com>
2024-05-08 16:35:10 +0800

    Create DFS.cpp

---
+++ DFS.cpp
@@ -0,0 +1,19 @@
+//DFS實作
+void dfs(int x, int y)
+{
+int i, skip = 0;
+int nextX, nextY;
+  if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT || visit[x][y] == 1) {
+    return;
+  }
+visit[x][y] = 1;
+printf("visit: %d, %d\n", x, y);
+  for (i=0; i<DIRECTIONS; i++) {
+  nextX = x + dir[i][0];
+  nextY = y + dir[i][1];
+  dfs(nextX, nextY);
+  }
+}
+dir[
+4][2] ={{0,1},{1,0},{0,-1},{-1,0}};
+
