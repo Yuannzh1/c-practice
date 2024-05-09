e3b568ea97079e1ad7b6b2fcc47a7bb980ae8cf1
Yuannzh1 <165546950+Yuannzh1@users.noreply.github.com>
2024-05-09 14:31:10 +0800

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,18 +1,39 @@
-//DFS實作
-void dfs(int x, int y)
-{
-int i, skip = 0;
-int nextX, nextY;
-  if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT || visit[x][y] == 1) {
-    return;
-  }
-visit[x][y] = 1;
-printf("visit: %d, %d\n", x, y);
-  for (i=0; i<DIRECTIONS; i++) {
-  nextX = x + dir[i][0];
-  nextY = y + dir[i][1];
-  dfs(nextX, nextY);
-  }
-}
-dir[4][2] ={{0,1},{1,0},{0,-1},{-1,0}};
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+// 深度優先搜尋
+void dfs(vector<vector<int>>& graph, int node, vector<bool>& visited) {
+    if (!visited[node - 1]) {
+        cout << node << endl;
+        visited[node - 1] = true;
+        for (size_t i = 0; i < graph[node - 1].size(); ++i) {
+            if (graph[node - 1][i] == 1 && !visited[i]) {
+                dfs(graph, i + 1, visited);
+            }
+        }
+    }
+}
+
+// 主程式
+int main() {
+    int N;
+    cin >> N;
+
+    // 讀取圖型相鄰矩陣表示法
+    vector<vector<int>> graph(N, vector<int>(N));
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cin >> graph[i][j];
+        }
+    }
+
+    // 用來記錄節點是否已被訪問
+    vector<bool> visited(N, false);
+
+    // 從第一個節點開始DFS
+    dfs(graph, 1, visited);
+
+    return 0;
+}
